e0be00104b401ed4c9d06335734d0eb7cc0f5e34
Tommy Pang <tommypang04@gmail.com>
2023-08-26 15:43:32 -0400

    implmeneted image zoom in & out

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -11,6 +11,8 @@ import {MatInputModule} from "@angular/material/input";
 import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {UserProfileComponent} from "@app/components/user-profile/user-profile.component";
+import {NgxImageZoomModule} from "ngx-image-zoom";
 
 @NgModule({
   declarations: [
@@ -18,8 +20,9 @@ import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/
     NavbarComponent,
     PatientDetailComponent,
     LoginComponent,
+    UserProfileComponent
   ],
-  imports: [CommonModule, RouterModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatTableModule, MatSortModule, MatDialogModule],
+  imports: [CommonModule, RouterModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatTableModule, MatSortModule, MatDialogModule, NgxImageZoomModule],
   exports: [
     NavbarComponent
   ],
