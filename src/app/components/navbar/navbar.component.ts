b4a9e51d866170e6d5d4c1072bab04c42517dcc2
gracexuwt <gracexuwt1126@gmail.com>
2023-09-04 19:58:26 -0400

    completed implementation of email upload

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component,ElementRef } from '@angular/core';
+import { Component} from '@angular/core';
 import {AccountService} from "@app/services";
 import { ReportService } from '@app/services/report.service';
 import { MatDialog } from '@angular/material/dialog';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { User } from '@app/models';
 import Swal from "sweetalert2";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-navbar',
@@ -16,7 +17,7 @@ export class NavbarComponent {
   issueDescription: string = '';
 
   constructor(public dialog: MatDialog,private accountService: AccountService,
-    private reportService: ReportService, private elementRef: ElementRef) {
+    private reportService: ReportService) {
     this.user = this.accountService.userValue;
   }
   logout() {
@@ -33,35 +34,54 @@ export class NavbarComponent {
   }
 
   async fileReport() {
-    const screenshotData = await this.reportService.captureScreenshot(this.elementRef.nativeElement);
-    const { value: issueDescription } = await Swal.fire({
-      title: 'Screenshot saved. \n Please enter the issue description.',
-      input: 'text',
-      inputPlaceholder: 'Type your issue description here',
-      inputAttributes: {
-        required: 'true'
-      },
-      showCancelButton: true,
-      confirmButtonText: 'Submit Issue Report',
-      cancelButtonText: 'Cancel Issue Report',
-      inputValidator: (value) => {
-        if (!value) {
-          return 'Issue description is required';
+    try {
+      const canvas = await html2canvas(document.body); // capture entire viewport
+      const screenshotData = canvas.toDataURL('image/png');
+
+      const screenshotFile = this.reportService.generateFile(screenshotData);
+  
+      const { value: issueDescription } = await Swal.fire({
+        title: 'Screenshot saved. \n Please enter the issue description.',
+        input: 'text',
+        inputPlaceholder: 'Type your issue description here',
+        inputAttributes: {
+          required: 'true'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Submit Issue Report',
+        cancelButtonText: 'Cancel Issue Report',
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Issue description is required';
+          }
+          return undefined;
         }
-        return undefined;
+      });
+  
+      if (issueDescription && this.user && this.user.id) {
+        // send request to report service
+        try {
+          await this.reportService.sendReport(screenshotFile, issueDescription, this.formatDateTime(new Date()), this.user.id);
+          Swal.fire("Success", "Report filed successfully.", "success");
+        } catch (error) {
+          console.log(error);
+          Swal.fire("Error", "An error occurred while filing report.", "error");
+        }
       }
-    });
-    if(issueDescription) {
-      const reportData = this.reportService.prepareReport(issueDescription, screenshotData);
-      console.log(reportData);
-      //send request to report service
-      try {
-        await this.reportService.sendReport(reportData);
-        Swal.fire("Success", "Report filed successfully.", "success");
-      } catch (error) {
-        console.log(error);
-        Swal.fire("Error", "An error occurred while filing report.", "error");
-      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire('Error', 'An error occurred while capturing the screenshot.', 'error');
     }
+  }
+
+  formatDateTime(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
 }
